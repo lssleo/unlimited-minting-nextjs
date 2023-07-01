0956caefadc9d0235bf7ee2cdfd53ff39510dd01
Leonid Shaydenko <103574243+lssleo@users.noreply.github.com>
2023-07-01 21:59:42 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,25 @@ import Link from 'next/link'
 
 const Header = () => (
     <nav className="p-5 border-b-2 border-color-white flex flex-row justify-between items-center">
-        <h1 className="py-4 px-4 font-bold text-3xl center text-white">UNLIMITED</h1>
+        <h1 className="py-2 px-2 font-bold text-1l center text-white sm:hidden">UNLIM</h1>
+        <h1 className="py-2 px-2 font-bold text-1l center text-white hidden sm:block">UNLIMITED</h1>
         <div className="flex flex-row items-center">
             <div className="flex justify-start px-4">
                 <Link href="/">
-                    <h2 className="px-4 text-xl text-white">MINT NFT</h2>
+                    <h2 className="px-2 text-s text-white sm:hidden">MINT</h2>
+                    <h2 className="px-2 text-s text-white hidden sm:block">MINT NFT</h2>
                 </Link>
                 <Link href="/soulbound">
-                    <h2 className="px-4 text-xl text-white">SOULBOUND</h2>
+                    <h2 className="px-2 text-s text-white sm:hidden">SBT</h2>
+                    <h2 className="px-2 text-s text-white hidden sm:block">SOULBOUND</h2>
                 </Link>
             </div>
-            <ConnectButton />
+            <div className="hidden sm:block">
+                <ConnectButton label="Connect" />
+            </div>
+            <div className="block sm:hidden">
+                <ConnectButton label="Co..." />
+            </div>
         </div>
     </nav>
 )
